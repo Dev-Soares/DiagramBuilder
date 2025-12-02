58310033bdf1e9ec4f357329f8be3abe09cc969e
Dev-Soares <bernardo.soares30@outlook.com>
2025-12-02 11:58:42 -0300

    feat: ajustes no tutorial

--- frontend/src/components/smallComponents/TutorialButton.jsx
+++ frontend/src/components/smallComponents/TutorialButton.jsx
@@ -13,7 +13,7 @@ const TutorialButton = () => {
         <div className="flex flex-col justify-start items-start mb-6 gap-2 opacity-0 animate-fadeIn w-full relative">
             <button onClick={() => startTutorial(screenType)} className='w-full h-auto flex justify-start items-center gap-1 md:gap-2 lg:gap-3 text-yellow-400 font-bold py-2 px-3 cursor-pointer text-sm xl:text-lg
                         hover:text-white hover:translate-y-[-2px] transition-all duration-600'>
-                <span className='material-symbols-outlined'>help</span> Como Funciona
+                <span className='material-symbols-outlined'>help</span> Tutorial
             </button>
         </div>
     )
--- frontend/src/utils/tutorial.js
+++ frontend/src/utils/tutorial.js
@@ -13,14 +13,14 @@ const startTutorial = (screenType) => {
                     intro: "Esta é a área de trabalho onde você pode arrastar e soltar elementos para criar seu diagrama.",
                 },
                 {
-                    element: document.querySelector('#edit-area'),
-                    intro: "Aqui está a barra lateral de edição. Você pode alterar o nome do projeto e salvar seu progresso, entre outras funções.",
-                    position: 'bottom'
-                },
-                {
                     element: document.querySelector('#Stacks'),
                     intro: "Estes são alguns dos vários elementos que podem ser inseridos no seu projeto. Clique e arraste qualquer elemento até a área de trabalho para começar a construir seu projeto!",
                     position: 'right'
+                },
+                {
+                    element: document.querySelector('#edit-area'),
+                    intro: "Aqui está a barra lateral de edição. Você pode alterar o nome do projeto e salvar seu progresso, entre outras funções.",
+                    position: 'bottom'
                 },
                 {
                     element: document.querySelector('#edit-area'),
@@ -40,13 +40,13 @@ const startTutorial = (screenType) => {
                     intro: "Esta é a área de trabalho onde você pode colocar elementos para criar seu diagrama.",
                 },
                 {
+                    element: document.querySelector('#Stacks'),
+                    intro: " Clique em algum elemento para selecioná-lo e depois toque na área de trabalho para adicioná-lo ao seu projeto!",
+                },
+                {
                     element: document.querySelector('#edit-area'),
                     intro: "Aqui está a barra lateral de edição. Você pode alterar o nome do projeto e salvar seu progresso, entre outras funções.",
                     position: 'bottom'
-                },
-                {
-                    element: document.querySelector('#Stacks'),
-                    intro: " Clique em algum elemento para selecioná-lo e depois toque na área de trabalho para adicioná-lo ao seu projeto!",
                 },
                 {
                     element: document.querySelector('#edit-area'),
